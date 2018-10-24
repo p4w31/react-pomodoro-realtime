31997d7dd7cd4ce88480090582a6c151785f59a6
Pawel <szalewskipawel@gmail.com>
2018-10-24 10:10:17 +0200

    heroku fix

---
+++ src/config/prod.js
@@ -0,0 +1,5 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyBm30a-a89dk6VDyTFBajecidFVBfm-Pzo",
+    authDomain: "pomodoro-realtime.firebaseapp.com",
+    databaseURL: "https://pomodoro-realtime.firebaseio.com"
+};
